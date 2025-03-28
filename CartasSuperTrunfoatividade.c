5971f998e1e972af61ddd248853980646b5ce360
biancafeil <biancafeil.contato@gmail.com>
2025-03-28 01:57:28 +0000

    adcionei as variaveis

---
+++ CartasSuperTrunfoatividade.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+    char estado;
+    char código[6];
+    char cidade[50];
+    int populacao;
+    float área;
+    float pib;
+    int quantidade; //quantidade de pontos turísticos
+
+    
+
+
+
+
+
+
+
+}
